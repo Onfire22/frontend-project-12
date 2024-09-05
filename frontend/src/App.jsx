21d12476c754e95d390288484d15bcb9ea3b7597
maxim <maddmax22@yandex.ru>
2024-09-05 18:45:30 +0300

    added base routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import { ROUTES } from './routes/routes';
 import Login from './pages/Login';
+import Chat from './pages/Chat';
+import NotFound from './pages/404';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Login />
+      <Routes>
+        <Route path={ROUTES.chat} element={<Chat />} />
+        <Route path={ROUTES.login} element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 };
---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,14 @@
+const API_ROUTES = {
+  login: '/api/v1/login',
+  signup: '/api/v1/signup',
+  getChannels: '/api/v1/channels',
+  getMessages: '/api/v1/messages',
+};
+
+const ROUTES = {
+  chat: '/',
+  login: '/login',
+  signup: '/signup',
+};
+
+export { API_ROUTES, ROUTES };
