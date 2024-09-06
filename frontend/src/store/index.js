2be801c72b180ca9ebb3dcf259fcde296a29fa13
maxim <maddmax22@yandex.ru>
2024-09-06 16:36:54 +0300

    added channels slice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './slices/authSlice';
+import channelsSlice from './slices/channelsSlice';
 
 export default configureStore({
   reducer: {
     auth: authSlice,
+    channels: channelsSlice,
   },
 });
---
+++ frontend/src/store/slices/channelsSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { API_ROUTES } from '../../routes/routes';
+
+const initialState = {
+  channels: [],
+  status: 'idle',
+  errors: null,
+};
+
+const fetchChannels = createAsyncThunk(
+  'channels/fetchAll',
+  async (_, {
+    getState,
+  }) => {
+    const { token } = getState().auth;
+    const response = await axios.get(API_ROUTES.getChannels, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  },
+);
+
+const channelsSlice = createSlice({
+  name: 'channels',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchChannels.pending, (state) => {
+        state.status = 'pending';
+        state.errors = null;
+      })
+      .addCase(fetchChannels.fulfilled, (state, { payload }) => {
+        state.status = 'idle';
+        state.errors = null;
+        state.channels = payload;
+      })
+      .addCase(fetchChannels.rejected, (state, action) => {
+        console.log(action.error.message);
+        state.status = 'failed';
+        state.errors = action.error.message;
+      });
+  },
+});
+
+export { fetchChannels };
+export default channelsSlice.reducer;
