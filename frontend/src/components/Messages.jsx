d0df6643379728d3445223f6b778d3dca581b050
maxim necrasov <maddmax22@yandex.ru>
2024-09-07 14:18:06 +0300

    added spinners for channels

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,15 +3,13 @@ import Message from './Message';
 import MessagesSpinner from './MessagesSpinner';
 
 const Messages = () => {
-  const messages = useSelector((state) => state.messages.messages);
-  const status = useSelector((state) => state.messages.status);
-
+  const messages = useSelector((state) => state.messages);
   return (
-    status === 'pending'
+    messages.status === 'pending'
       ? <MessagesSpinner />
       : (
         <div className="chat-messages overflow-auto px-5" id="messages-box">
-          {!!messages.length && messages.map((message) => {
+          {!!messages.messages.length && messages.messages.map((message) => {
             return <Message key={message.id} text={message.text} />;
           })}
         </div>
