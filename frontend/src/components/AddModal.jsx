a91ce9c2a5a80f8833d6d063f7f7c619ef0f6c0a
maxim <maddmax22@yandex.ru>
2024-09-12 15:18:08 +0300

    add validation for channel name

--- frontend/src/components/AddModal.jsx
+++ frontend/src/components/AddModal.jsx
@@ -1,4 +1,6 @@
-import { useDispatch } from 'react-redux';
+import * as yup from 'yup';
+import cn from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -6,16 +8,38 @@ import Modal from 'react-bootstrap/Modal';
 import { createChannel, setActive } from '../store/slices/channelsSlice';
 
 const AddModal = ({ show, toggleModal }) => {
+  const channels = useSelector((state) => state.channels.channels);
   const dispatch = useDispatch();
+
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .required('Обязательное поле')
+      .min(2, 'От 3 до 20 символов')
+      .max(20, 'От 3 до 20 символов')
+      .notOneOf(channels, 'Должно быть уникальным'),
+  });
+
   const formik = useFormik({
     initialValues: {
       name: '',
     },
+    validationSchema: schema,
     onSubmit: (values) => {
-      dispatch(createChannel(values.name));
-      dispatch(toggleModal());
-      dispatch(setActive(values.name));
+      schema.validate(values)
+        .then(() => {
+          dispatch(createChannel(values.name));
+          dispatch(toggleModal());
+          dispatch(setActive(values.name));
+        })
+        .catch((e) => {
+          formik.errors.name = e.message;
+        });
     },
+  });
+
+  const inputClasses = cn('mb-2', {
+    'form-control is-invalid': formik.errors.name,
   });
 
   return (
@@ -27,12 +51,14 @@ const AddModal = ({ show, toggleModal }) => {
         <Form onSubmit={formik.handleSubmit}>
           <Form.Group className="mb-3" id="name" controlId="name">
             <Form.Control
+              className={inputClasses}
               type="text"
               name="name"
               value={formik.values.text}
               onChange={formik.handleChange}
             />
             <Form.Label className="visually-hidden">Имя канала</Form.Label>
+            <div className="invalid-feedback">{formik.errors.name}</div>
           </Form.Group>
           <div className="d-flex justify-content-end">
             <Button className="me-2 btn-secondary" type="button">Отменить</Button>
--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -2,20 +2,40 @@ import cn from 'classnames';
 import { useDispatch, useSelector } from 'react-redux';
 import { setActive } from '../store/slices/channelsSlice';
 
-const Channel = ({ name }) => {
+const Channel = ({ name, removable }) => {
   const dispatch = useDispatch();
   const active = useSelector((state) => state.channels.activeChannel);
 
-  const btnClass = cn('w-100', 'rounded-0', 'text-start', 'btn', {
+  const btnActiveClass = cn('w-100', 'rounded-0', 'text-start', 'btn', {
+    'btn-secondary': active === name,
+  });
+
+  const btnEditClass = cn('flex-grow-0', 'dropdown-toggle', 'dropdown-toggle-split', 'btn', {
     'btn-secondary': active === name,
   });
 
   return (
     <li className="nav-item w-100">
-      <button className={btnClass} type="button" onClick={() => dispatch(setActive(name))}>
-        <span className="me-1">#</span>
-        {name}
-      </button>
+      <div className="d-flex dropdown btn-group" role="group">
+        <button className={btnActiveClass} type="button" onClick={() => dispatch(setActive(name))}>
+          <span className="me-1">#</span>
+          {name}
+        </button>
+        {
+          removable && (
+          <button
+            className={btnEditClass}
+            type="button"
+            id="react-aria2952926309-:r0:"
+            aria-expanded="false"
+          >
+            <span className="visually-hidden">
+              Управление каналом
+            </span>
+          </button>
+          )
+        }
+      </div>
     </li>
   );
 };
