f26570a11426b59ff878b961c38be6ac5149c2e4
maxim <maddmax22@yandex.ru>
2024-09-24 15:11:02 +0300

    add rollbar

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './store/index.js';
 import i18n from './init.js';
+import rollbarConfig from './helpers/rollbarConfig.js';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </Provider>
-  </BrowserRouter>,
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <App />
+          </I18nextProvider>
+        </Provider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </RollbarProvider>,
 );
