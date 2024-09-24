eb90ccf9e36b857376f77359a5b882053c7ba1f5
maxim <maddmax22@yandex.ru>
2024-09-24 19:02:02 +0300

    fixes for linter

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -3,14 +3,15 @@ import Channel from './Channel';
 import Loader from './Loader';
 
 const Channels = () => {
-  const channels = useSelector((state) => state.channels);
+  const channels = useSelector((state) => state.channels.channels);
+  const status = useSelector((state) => state.channels.status);
 
   return (
-    channels.status === 'pending'
+    status === 'pending'
       ? <Loader text="каналов" />
       : (
         <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block" id="channels-box">
-          {!!channels.channels.length && channels.channels.map((channel) => {
+          {!!channels.length && channels.map((channel) => {
             return (
               <Channel
                 key={channel.id}
--- frontend/src/store/slices/channelsSlice.js
+++ frontend/src/store/slices/channelsSlice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { API_ROUTES } from '../../routes/routes';
