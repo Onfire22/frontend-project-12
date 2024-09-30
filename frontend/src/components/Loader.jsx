cd2f803e0f09188f76f2220c3fdef8eecbf76d6b
maxim <maddmax22@yandex.ru>
2024-09-30 14:26:17 +0300

    fix i18 for loader

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,10 @@
 import Spinner from 'react-bootstrap/Spinner';
 
-const Loader = ({ translate = () => {}, text }) => {
-  return (
-    <div className="h-100 d-flex flex-column align-items-center justify-content-center">
-      <p>{`${translate('loader.loading')} ${text}`}</p>
-      <Spinner animation="border" variant="primary" />
-    </div>
-  );
-};
+const Loader = ({ translate = () => {}, text }) => (
+  <div className="h-100 d-flex flex-column align-items-center justify-content-center">
+    <p>{`${translate('loader.loading')} ${text}`}</p>
+    <Spinner animation="border" variant="primary" />
+  </div>
+);
 
 export default Loader;
